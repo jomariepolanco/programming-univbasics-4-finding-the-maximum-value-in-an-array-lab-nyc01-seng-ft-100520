8c31c91943633af7b528a8a8aafbb8b8cc370bc1
Jomarie Polanco <jomariepolanco@gmail.com>
2020-09-12 10:53:07 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,31 +1,27 @@
-#def find_max_value(array)
-  # Add your solution here
- # i = 0
-  #sort_array = array.sort 
-  #while i < array.length do 
-   # i += 1
-  #end 
-  #return sort_array.last 
-#end
+def find_max_value(array)
+   #Add your solution here
+  i = 0
+  sort_array = array.sort 
+  while i < array.length do 
+    i += 1
+  end 
+  return sort_array.last 
+end
 
 #finds the element in an array with the highest value 
 #iterate through array
 #sort element
 #return last element using .last 
 
-#def find_max_value(array)
- # i = 0 
-  #max_value = -1
-  #while i < array.length do
-   # if max_value < array[i]
-   #   max_value = array[i]
-    #end
-    #i += 1 
-  #end 
-  #max_value
-#end 
-
 def find_max_value(array)
-  sort_array = array.sort 
-  sort_array.length.times do {|i| sort_array.last[i]}
-end
+  i = 0 
+  max_value = -1
+  while i < array.length do
+    if max_value < array[i]
+      max_value = array[i]
+    end
+    i += 1 
+  end 
+  max_value
+end 
+
